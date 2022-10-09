b07558b57b84c86bd5a177da4a3b7af3222067f2
Haseop Lee <haservi@naver.com>
2022-10-09 23:27:01 +0900

    docs(README): 기능 목록 체크 및 주석 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,6 @@ import racingcar.view.GameView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         GameController gameController = new GameController(new GameView());
         gameController.playing();
     }
